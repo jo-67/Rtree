d601cbdee6b2e4e9222ffddc58f89252ae0ca650
Jose Vera <81767344+jose-vera-uchile@users.noreply.github.com>
2023-10-11 19:10:30 -0300

    STR terminado y creacion de Tests para STR. Falta una clase que compare dos RTrees para poder comparar dos y ver si la creacion de los nodos es igual. Tambien se podria hacer una funcion de print para visualizarlos

--- src/STR.java
+++ src/STR.java
@@ -20,19 +20,19 @@ public class STR {
         this.maxChildren = MaxChildren;
     }
 
-    public RTree createSTRRTree() {
+    public RTree createRTree() {
         // n rectangulos, M cantidad maxima de hijos
-        // ordena rectangulos en base a x del centro
+        // ordena rectangulos con respecto a x del centro
         // junta en S grupos, con S = sqrt(S/M)
-        // cada grupo se ordena en base a la Y del centro
-        // Se agrupan en grupos de tamaño M (a excepción del ultimo grupo)
+        // cada grupo se ordena con respecto a la Y del centro
+        // Se agrupan en grupos de tamaño M (a excepción del último grupo)
         // Se obtienen S*S grupos de tamaño máximo M
         // Ahora teniendo los grupos, se crea una nueva lista de rectángulos con el MBR de cada grupo
         // Se repite el procedimiento hasta que se puedan agrupar todos los rectángulos en un solo nodo
 
         orderByCenterX(rectangleList);
 
-        int S = (int) Math.ceil(Math.sqrt(rectangleList.size()/maxChildren)); //Calculamos la raiz y tomamos el ceiling
+        int S = (int) Math.ceil(Math.sqrt((double) rectangleList.size() /maxChildren)); //Calculamos la raiz y tomamos el ceiling
 
         // Para separar los rectangulos, lo haremos en lista de listas. Cada elemento de esta lista corresponderá
         // a uno de los subgrupos de puntos, con los que luego se crearan los nodos. Estas listas no son nodos ni
@@ -40,7 +40,7 @@ public class STR {
 
         List<List<Rectangle>> subgroupsList = new ArrayList<List<Rectangle>>();
         List<Rectangle> currentSubgroup = new ArrayList<Rectangle>();
-        int n=0; //Contador de cuantos rectangulos llevamos en cada lista
+        int n=0; //Contador de cuantos rectángulos llevamos en cada lista
         for (int i=0; i < rectangleList.size();i++){
             if (n==S*maxChildren) { // Si llegamos al maximo de valores en un subgrupo
                 orderByCenterY(currentSubgroup); //aprovecho de ordenarlos por Y antes de agregarlos a la lista de lista
@@ -62,10 +62,10 @@ public class STR {
 
         List<INode> nodes = new ArrayList<>(); //Acá guardamos los nodos que se van creando
         Node current = new Node(); //Este sera el nodo actual
-        int m = 0; //Numero de rectangulos en el nodo actual
+        int m = 0; //Número de rectangulos en el nodo actual
 
 
-        //Este codigo es casi el mismo de antes, solo que recorre por subgrupos. Tambien es muy similar al de NearestX
+        //Este código es casi el mismo de antes, solo que recorre por subgrupos. Tambien es muy similar al de NearestX
 
         for (int i = 0; i< subgroupsList.size(); i++){ //recorremos cada sublista de la lista de grupos
             for (int j = 0; j < subgroupsList.get(i).size(); j++){ //recorremos toda la sublista
@@ -75,52 +75,83 @@ public class STR {
                     nodes.add(current);
                     current = new Node();
                 }
+                if (m==0){
+                    current = new Node();
+                }
                 current.addLeaf(subgroupsList.get(i).get(j));//Accedemos al rectangulo actual de la lista actual
                 m++;
             }
             current.createMbr();
             nodes.add(current); // añade el ultimo
-            current = new Node();
             m = 0;
         }
 
 
-        if (rectangleList.size() < maxChildren) {
-            // todos estan dentro de un nodo
+        if (rectangleList.size() <= maxChildren) {
+            // Este es el caso base de la recursion
             return new RTree(current, maxChildren);
         }
         // sino, debe seguir agrupando a los nodos
 
-        return groupNodes(nodes);
+        return groupNodes(nodes); //Este es el caso recursivo
     }
     public RTree groupNodes(List<INode> nodeList) {
-        if (nodeList.size() < maxChildren) {
+        if (nodeList.size() <= maxChildren) {
             // todos estan dentro de un nodo
             Node root = new Node(nodeList);
             return new RTree(root, maxChildren);
         }
 
-        orderByNodeCenterX(nodeList);
-        List<INode> children = new ArrayList<>();
-        Node current = null;
+
+        orderByNodeCenterX(nodeList); //Ordenamos los nodos por el centro del MBR que representan
+        //Ahora debemos repetir el procedimiento para los MBR de los nodos como se hizo para los rectángulos
+        int S= (int) Math.ceil(Math.sqrt((double) nodeList.size() /maxChildren)); //Calculamos la raíz y tomamos el ceiling
+
+        List<List<INode>> subgroupsList = new ArrayList<List<INode>>(); //Similar a lo de antes, creamos una lista de sublistas
+        List<INode> currentSubgroup = new ArrayList<INode>();
+        int n = 0;
+
+        for (int i = 0; i< nodeList.size(); i++){
+            if (n==S*maxChildren) { // Si llegamos al maximo de valores en un subgrupo
+                orderByNodeCenterY(currentSubgroup); //aprovecho de ordenarlos por Y antes de agregarlos a la lista de lista
+                subgroupsList.add(currentSubgroup); //agregamos la lista actual a la lista de listas
+                currentSubgroup = new ArrayList<INode>(); //Reiniciamos la lista
+                n=0; //reiniciamos el contador
+            }
+            currentSubgroup.add(nodeList.get(i)); //Agregamos el valor rectangulo al grupo actual
+            n++; //aumentamos 1 el contador de rectangulos en el grupo actual
+        }
+
+        orderByNodeCenterY(currentSubgroup); //aprovecho de ordenarlos por Y antes de agregarlos a la lista de lista
+        subgroupsList.add(currentSubgroup); //agregamos la lista actual a la lista de listas
+
+        //Ahora creamos los nuevos nodos con la lista de nodos
+
+        List<INode> parents = new ArrayList<>();
+        Node current = new Node();
         int m = 0;
-        for (int i = 0; i < nodeList.size(); i++) {
-            if (m == maxChildren) {
-                m = 0;
-                current.createMbr(); // Crea el rectangulo MBR
-                children.add(current);
+        for (int i = 0; i < subgroupsList.size(); i++) {
+            for (int j = 0; j < subgroupsList.get(i).size(); j++){ //recorremos toda la sublista
+                if (m == maxChildren) {
+                    m = 0;
+                    current.createMbr(); // Crea el rectangulo MBR
+                    parents.add(current);
+                    current = new Node();
+                }
+                if (m==0){
+                    current = new Node();
+                }
+                current.addChild(subgroupsList.get(i).get(j));//Accedemos al rectangulo actual de la lista actual
+                m++;
             }
-            if (m==0) {
-                current = new Node();
-            }
-            current.addChild(nodeList.get(i));
-            m++;
-        }
-        if (current != null) {
             current.createMbr();
-            children.add(current); // añade el ultimo
+            parents.add(current); // añade el ultimo nodo que falta. Esto se hace en cada ciclo ya que normalmente sera un nodo de m=maxChildren que no hizo el ultimo ciclo.
+                                  // a excepcion del ultimo nodo del ultimo grupo, que podria tener menos e igual se debe agregar.
+            m = 0;
         }
-        return groupNodes(children);
+
+        //Ya que el caso base esta arriba, que seria cuando el numero de nodos de este nivel cabe en uno solo, entonces podemos proceder a agregar
+        return groupNodes(parents);
     }
     public void orderByCenterX(List<Rectangle> lRec) {
         Comparator<Rectangle> compareByCenterX =
--- test/STRTest.java
+++ test/STRTest.java
@@ -1,11 +1,154 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.w3c.dom.css.Rect;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class STRTest {
 
+    List<Rectangle> listaDeRectangulos = new ArrayList<>();
+    List<Rectangle> points = new ArrayList<>();
+    RTree testRTree = null;
+    @Before
+    public void prep() {
+        Rectangle r1 = new Rectangle(1, 2, 3, 4);
+        Rectangle r2 = new Rectangle(5, 3, 7, 6);
+        Rectangle r3 = new Rectangle(2, 1, 4, 5);
+        Rectangle r4 = new Rectangle(3, 4, 6, 8);
+        Rectangle r5 = new Rectangle(2, 2, 5, 5);
+        Rectangle r6 = new Rectangle(6, 2, 8, 4);
+        Rectangle r7 = new Rectangle(1, 5, 3, 7);
+        Rectangle r8 = new Rectangle(4, 1, 6, 3);
+        Rectangle r9 = new Rectangle(7, 5, 9, 8);
+        Rectangle r10 = new Rectangle(2, 6, 5, 9);
+        listaDeRectangulos.add(r1);
+        listaDeRectangulos.add(r2);
+        listaDeRectangulos.add(r3);
+        listaDeRectangulos.add(r4);
+        listaDeRectangulos.add(r5);
+        listaDeRectangulos.add(r6);
+        listaDeRectangulos.add(r7);
+        listaDeRectangulos.add(r8);
+        listaDeRectangulos.add(r9);
+        listaDeRectangulos.add(r10);
+
+        //Usare estos cuadrados para probar, que serán puntos
+        Rectangle r11 = new Rectangle(1, 1, 1, 1);
+        Rectangle r12 = new Rectangle(1, 2, 1, 2);
+        Rectangle r13 = new Rectangle(1, 3, 1, 3);
+        Rectangle r14 = new Rectangle(1, 4, 1, 4);
+        Rectangle r21 = new Rectangle(2, 1, 2, 1);
+        Rectangle r22 = new Rectangle(2, 2, 2, 2);
+        Rectangle r23 = new Rectangle(2, 3, 2, 3);
+        Rectangle r24 = new Rectangle(2, 4, 2, 4);
+        Rectangle r31 = new Rectangle(3, 1, 3, 1);
+        Rectangle r32 = new Rectangle(3, 2, 3, 2);
+        Rectangle r33 = new Rectangle(3, 3, 3, 3);
+        Rectangle r34 = new Rectangle(3, 4, 3, 4);
+        Rectangle r41 = new Rectangle(4, 1, 4, 1);
+        Rectangle r42 = new Rectangle(4, 2, 4, 2);
+        Rectangle r43 = new Rectangle(4, 3, 4, 3);
+        Rectangle r44 = new Rectangle(4, 4, 4, 4);
+        points.add(r11);
+        points.add(r12);
+        points.add(r13);
+        points.add(r14);
+        points.add(r21);
+        points.add(r22);
+        points.add(r23);
+        points.add(r24);
+        points.add(r31);
+        points.add(r32);
+        points.add(r33);
+        points.add(r34);
+        points.add(r41);
+        points.add(r42);
+        points.add(r43);
+        points.add(r44);
+
+        //Crearé el Rtree a mano para ver que funcione correctamente:
+        //Crearé uno de hijos máximo 4, con 16 nodos. Ya que son puntos es facil ver como deberia quedar.
+        //Partimos creando los nodos que apuntan a cada rectángulo
+        Node n1 = new Node();
+        Node n2 = new Node();
+        Node n3 = new Node();
+        Node n4 = new Node();
+
+        n1.addLeaf(r11); //Este rectangulo es el de abajo a la izquierda, debe tomar el 11, 12, 21, y 22
+        n1.addLeaf(r12);
+        n1.addLeaf(r21);
+        n1.addLeaf(r22);
+        n2.addLeaf(r13); //Este rectangulo es el de arriba a la izquierda, debe tomar el 13, 14, 23, y 24
+        n2.addLeaf(r14);
+        n2.addLeaf(r23);
+        n2.addLeaf(r24);
+        n3.addLeaf(r31); //Este rectangulo es el de abajo a la derecha, debe tomar el 31, 32, 41, y 42
+        n3.addLeaf(r32);
+        n3.addLeaf(r41);
+        n3.addLeaf(r42);
+        n4.addLeaf(r33); //Este rectangulo es el de arriba a la derecha, debe tomar el 33, 34, 43, y 44
+        n4.addLeaf(r34);
+        n4.addLeaf(r43);
+        n4.addLeaf(r44);
+
+        //finalmente, creamos un nuevo nodo que incluya solo estos 4 nodos. Para esto creamos una lista que
+        // tenga estos nodos, y creamos el Rtree con el nodo que recibe esta lista de base
+        List<INode> Nodos = new ArrayList<INode>();
+
+        Nodos.add(n1);
+        Nodos.add(n2);
+        Nodos.add(n3);
+        Nodos.add(n4);
+
+        Node root = new Node();
+        root.addChild(n1);
+        root.addChild(n2);
+        root.addChild(n3);
+        root.addChild(n4);
+
+        testRTree = new RTree(root, 4);
+
+
+
+    }
+
+    @Test
+    public void orderByCenterXTest() {
+        STR str = new STR(listaDeRectangulos, 3);
+        str.orderByCenterY(listaDeRectangulos);
+        List<Rectangle> nuevaLista = str.getRectangleList();
+        System.out.println(nuevaLista);
+
+
+        RTree rtree = str.createRTree();
+        System.out.println(rtree);
+
+    }
+
+    @Test
+    public void orderByCenterYTest() {
+        STR str = new STR(listaDeRectangulos, 3);
+        str.orderByCenterX(listaDeRectangulos);
+        List<Rectangle> nuevaLista = str.getRectangleList();
+        System.out.println(nuevaLista);
+    }
+
+
+    @Test
+    public void createRTreeTest() {
+        STR str = new STR(listaDeRectangulos, 3);
+        RTree rtree = str.createRTree();
+        System.out.println(rtree);
+
+        str = new STR(points, 4);
+        rtree = str.createRTree();
+        //assertEquals(testRTree,rtree); //Crear un metodo para comparar dos RTrees
+
+    }
 }
