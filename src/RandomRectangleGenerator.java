479ffced655242b03052b1069d489d682f112279
DiegoOpazoD <diegoopazod@gmail.com>
2023-10-12 00:13:55 -0300

    el generador de rectangulos puede hacer de más tamaños

--- src/RandomRectangleGenerator.java
+++ src/RandomRectangleGenerator.java
@@ -7,18 +7,24 @@ import java.util.Random;
         //generedor de numeros randoms
         //solo se agregan a la lista si x1-x2 entre 0 y 100 y1 - y2 entre 0 y 100 o inverso con x2 - x1 .. etc
 public class RandomRectangleGenerator {
-    public static void main(String[] args){
+    public static List<Rectangle> crearRectangulos (int cantidad, int tamano, int hilbert) {
         //random object
         Random ran = new Random();
-        int n = (int)Math.pow(2,10); //cantidad de rectangulos que se quieren generar
-        int hilbert = 16; //número de la curva (log2 punto maximo)
+        int n = (int)Math.pow(2,cantidad); //cantidad de rectangulos que se quieren generar
         List<Rectangle> listaDeRectangulos = new ArrayList<>(); //lista para guardar los rectangulos
         int k = 0;
         while (listaDeRectangulos.size() != n) {
 
-            int max = (500000/1000)*(k+1); //rangos más pequeños para que demore menos
-            int min = (500000/1000)*k; //se podria cambiar hasta dividirlo en 5000
+            int max = (500000 / 1000) * (k + 1);
+            int min = (500000 / 1000) * k;
 
+            int kmax = 1000;
+
+            if (tamano > 100) {
+                max = (500000 / 5) * (k + 1); //rangos más pequeños para que demore menos
+                min = (500000 / 5) * k; //se podria cambiar hasta dividirlo en 5000
+                kmax = 5;
+            }
             //generando numero entre 0 y 500000 des comentar esto para probar si se demora más o menos
             //int x1 = ran.nextInt(500000);
             //int x2 = ran.nextInt(500000);
@@ -32,7 +38,7 @@ public class RandomRectangleGenerator {
 
             //vemos si los numeros sirven
             if (
-                    ((x1-x2)<=100 && (x1-x2)>=-100) && ((y1-y2)<=100 && (y1-y2)>=-100)
+                    ((x1-x2)<=tamano && (x1-x2)>=-tamano) && ((y1-y2)<=tamano && (y1-y2)>=-tamano)
             )
             {
                 Rectangle r = new Rectangle(x1,y1,x2,y2,hilbert);
@@ -40,12 +46,11 @@ public class RandomRectangleGenerator {
                 System.out.println(listaDeRectangulos.size());
             }
 
-            if (k == 1000){
+            if (k == kmax){
                 k = 0;
             }else{
                 k = k + 1;
             }
-            System.out.println(k);
 
         }
         System.out.println("Se generaron los rectangulos");
@@ -54,16 +59,12 @@ public class RandomRectangleGenerator {
         System.out.println(listaDeRectangulos.get(765).x2);
         System.out.println(listaDeRectangulos.get(765).y2);
 
-        HilbertCurve nearest = new HilbertCurve(listaDeRectangulos, 100);
-        nearest.orderByHilbertCurve(listaDeRectangulos);
-        List<Rectangle> nuevaLista = nearest.getRectangleList();
-        System.out.println(nuevaLista);
-        for (int i = 0; i<nuevaLista.size(); i++ ){
-            System.out.println(nuevaLista.get(i).getHilbertCurvePosition());
-        }
+        return listaDeRectangulos;
+    }
 
-        RTree rtree = nearest.createRTreeH();
-        System.out.println(rtree);
+    public static void main(String[] args){
+        crearRectangulos(10,100,12);
+        crearRectangulos(10, 100000, 0);
     }
 
 }
