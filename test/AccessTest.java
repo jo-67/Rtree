1de2127f1f32e4571013ce0060b5ba5b475f8217
DiegoOpazoD <diegoopazod@gmail.com>
2023-10-16 11:20:24 -0300

    tests

--- test/AccessTest.java
+++ test/AccessTest.java
@@ -2,27 +2,86 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.Objects;
 
 public class AccessTest {
-    List<Rectangle> randomList;
-    Rectangle r1;
+    List<Rectangle> randomListQ; //lista de consultas tiene que ser igual para todas
+    List<List<Rectangle>> randomList; //tienen que ser las mismas para todos asi que hay que generarlas antes
     @Before
     public void prep() {
-        randomList = RandomRectangleGenerator.crearRectangulos(10, 100, 0);
-        r1 = randomList.get(500);
+        randomListQ = RandomRectangleGenerator.crearRectangulos(7,100000, 0); //lista de rectangulos de consultas Q (en vez de 100 son 128)
+        for (int i = 10; i <= 15; i++){ //cambiar el 15 a 25 despues
+            List<Rectangle>  r = RandomRectangleGenerator.crearRectangulos(i,100,0);
+            randomList.add(r);
+        }
+        //System.out.println(randomList.size());
     }
-    @Test
-    public void Nearest() {
-        Nearest nearest = new Nearest(randomList, 3);
+
+    public void Nearest(int k) {
+        List<Rectangle>r = randomList.get(k); //+1 porque si no no calza
+        Nearest nearest = new Nearest(r, 3);
         RTree rtree = nearest.createRTree();
 
         int accesos = 0;
-        for (int i = 0; i < randomList.size(); i++) {
-            SearchResult result = rtree.search(r1);
+        for (int i = 0; i < randomListQ.size(); i++) {
+            SearchResult result = rtree.search(randomListQ.get(i));
             accesos = accesos + result.getCounter();
         }
         long accessAverageTime = accesos / randomList.size();
         System.out.println(accessAverageTime);
+    }
+
+    public void STR(int k) {
+        List<Rectangle>r = randomList.get(k);
+        STR str = new STR(r, 3);
+        RTree rtree = str.createRTree();
+
+        int accesos = 0;
+        for (int i = 0; i < randomListQ.size(); i++) {
+            SearchResult result = rtree.search(randomListQ.get(i));
+            accesos = accesos + result.getCounter();
+        }
+        long accessAverageTime = accesos / randomList.size();
+        System.out.println(accessAverageTime);
+    }
+
+    public void Hilbert(int k) {
+        List<Rectangle>r = randomList.get(k);
+
+        //recorremos la lista y le agregamos a cada rectangulo el numero de hilbert log_base_2 = 500.000
+        //probablemte exista un metodo más eficiente
+        int hc = (int)Math.pow(2,19); //no estoy seguro que sea el numero correcto
+        for (int i = 0; i< Objects.requireNonNull(r).size(); i++){
+            r.get(i).n = hc;
+        }
+
+        for (int i = 0; i< randomListQ.size(); i++){
+            randomListQ.get(i).n = hc;
+        }
+
+
+        HilbertCurve hilbert = new HilbertCurve(r, 3);
+        RTree rtree = hilbert.createRTreeH();
+
+
+        int accesos = 0;
+        for (int i = 0; i < randomListQ.size(); i++) {
+            SearchResult result = rtree.search(randomListQ.get(i));
+            accesos = accesos + result.getCounter();
+        }
+        long accessAverageTime = accesos / randomList.size();
+        System.out.println(accessAverageTime);
+    }
+
+
+
+    @Test
+    public void Access() {
+        for (int i = 0; i <= 5; i++) { //cambiar de 0 a 15 despues
+            Nearest(i); //iteramos por cada lista de 2_10 a 2_25
+            STR(i);
+            Hilbert(i);
+        }
     }
 
 }
--- test/TiempoBusquedaTest.java
+++ test/TiempoBusquedaTest.java
@@ -2,36 +2,112 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Random;
 
 public class TiempoBusquedaTest {
-    List<Rectangle> randomList;
+    //List<Rectangle> randomList;
+    List<Rectangle> randomListQ; //lista de consultas tiene que ser igual para todas
+    List<List<Rectangle>> randomList; //tienen que ser las mismas para todos asi que hay que generarlas antes
     Rectangle r1;
     @Before
     public void prep() {
-        randomList = RandomRectangleGenerator.crearRectangulos(10, 100, 0);
-        r1 = randomList.get(500);
-    }
 
-    public void Nearest(int p) {
-        randomList = RandomRectangleGenerator.crearRectangulos(p, 100, 0);
-        Nearest nearest = new Nearest(randomList, 3);
+        randomListQ = RandomRectangleGenerator.crearRectangulos(7,100000, 0); //lista de rectangulos de consultas Q (en vez de 100 son 128)
+        for (int i = 10; i <= 15; i++){ //cambiar el 15 a 25 despues
+          List<Rectangle>  r = RandomRectangleGenerator.crearRectangulos(i,100,0);
+          randomList.add(r);
+          }
+        //System.out.println(randomList.size());
+        }
+
+    public void Nearest(int k) {
+        List<Rectangle> r = null; // esto esta para que no se queje intellij
+        if (randomList != null) {
+            r = randomList.get(k); //sacamos de la lista la lista de rectangulos
+        }else{
+            System.out.println("ocurrio un error");
+        }
+
+        Nearest nearest = new Nearest(r, 3);
+
         RTree rtree = nearest.createRTree();
 
         long startTime = System.nanoTime();
-        for (int i = 0; i < randomList.size(); i++) {
-            SearchResult result = rtree.search(r1);
+        for (int i = 0; i < randomListQ.size(); i++) {
+            SearchResult result = rtree.search(randomListQ.get(i)); //buscamos por cada rectangulo de Q
         }
         long endTime = System.nanoTime();
 
         long searchTime = endTime - startTime;
-        long searchAverageTime = searchTime / randomList.size();
+        long searchAverageTime = searchTime / randomListQ.size();
+        System.out.println(searchAverageTime);
+    }
+
+    public void STR(int k){
+        List<Rectangle> r = null; // esto esta para que no se queje intellij
+        if (randomList != null) {
+            r = randomList.get(k); //sacamos de la lista la lista de rectangulos
+        }else{
+            System.out.println("ocurrio un error");
+        }
+
+        STR str = new STR(r, 3);
+
+        RTree rtree = str.createRTree();
+
+        long startTime = System.nanoTime();
+        for (int i = 0; i < randomListQ.size(); i++) {
+            SearchResult result = rtree.search(randomListQ.get(i)); //buscamos por cada rectangulo de Q
+        }
+        long endTime = System.nanoTime();
+
+        long searchTime = endTime - startTime;
+        long searchAverageTime = searchTime / randomListQ.size();
+        System.out.println(searchAverageTime);
+    }
+
+    public void Hilbert(int k){
+        List<Rectangle> r = null; // esto esta para que no se queje intellij
+        if (randomList != null) {
+            r = randomList.get(k); //sacamos de la lista la lista de rectangulos
+        }else{
+            System.out.println("ocurrio un error");
+        }
+
+        //recorremos la lista y le agregamos a cada rectangulo el numero de hilbert log_base_2 = 500.000
+        //probablemte exista un metodo más eficiente
+        int hc = (int)Math.pow(2,19); //no estoy seguro que sea el numero correcto
+        for (int i = 0; i< Objects.requireNonNull(r).size(); i++){
+            r.get(i).n = hc;
+        }
+
+        for (int i = 0; i< randomListQ.size(); i++){
+            randomListQ.get(i).n = hc;
+        }
+
+        HilbertCurve hilbert = new HilbertCurve(r, 3);
+
+        RTree rtree = hilbert.createRTreeH();
+
+        long startTime = System.nanoTime();
+        for (int i = 0; i < randomListQ.size(); i++) {
+            SearchResult result = rtree.search(randomListQ.get(i)); //buscamos por cada rectangulo de Q
+        }
+        long endTime = System.nanoTime();
+
+        long searchTime = endTime - startTime;
+        long searchAverageTime = searchTime / randomListQ.size();
         System.out.println(searchAverageTime);
     }
 
     @Test
     public void Tiempos() {
-        for (int i = 10; i <= 25; i++) {
-            Nearest(i);
+        for (int i = 0; i <= 15; i++) {
+            Nearest(i); //iteramos por cada lista de 2_10 a 2_25
+            STR(i);
+            Hilbert(i);
         }
     }
+
 }
